e3e5bb3f223ad5033405175779b630021096b460
dycat <8856528+dycat@users.noreply.github.com>
2023-05-28 15:56:26 +0800

    fix: uncomment build command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ OUTPUT_DIR=${GITHUB_WORKSPACE}/$INPUT_SOURCE/output
 CONFIG_DIR=${GITHUB_WORKSPACE}/$INPUT_SOURCE/pelicanconf.py
 
 echo "Generate the site $SOURCE_DIR $CONFIG_DIR"
-# pipenv run pelican $SOURCE_DIR -s $CONFIG_DIR -o $OUTPUT_DIR
+pipenv run pelican $SOURCE_DIR -s $CONFIG_DIR -o $OUTPUT_DIR
 # pipenv run pelican ./test_projects/hello/content
