6d2f03a4af0e892e6bcf598cad864bedd797e553
dycat <8856528+dycat@users.noreply.github.com>
2023-05-28 16:18:16 +0800

    fix: add markdown support

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ CONFIG_DIR=${GITHUB_WORKSPACE}/$INPUT_SOURCE/pelicanconf.py
 echo "Init the virtualenv and dependencies"
 pipenv install
 
-echo "Generate the site $SOURCE_DIR $CONFIG_DIR"
+echo "Generate the site $SOURCE_DIR with config $CONFIG_DIR"
 
 # pipenv run pelican $SOURCE_DIR -s $CONFIG_DIR -o $OUTPUT_DIR
-pipenv run pelican ./test_projects/hello/content
+pipenv run pelican /test_projects/hello/content
