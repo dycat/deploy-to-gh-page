1ae88f4cd0f508647fc87fb2f711cdfdf4d538a2
dycat <8856528+dycat@users.noreply.github.com>
2023-06-06 20:57:35 +0800

    Update publishconf.py

--- test_projects/hello/publishconf.py
+++ test_projects/hello/publishconf.py
@@ -4,7 +4,7 @@
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
+from test_projects.hello.pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = ''
